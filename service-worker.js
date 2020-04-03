d8cda8a80f2869dd5af9be7c2b0ea6d6a9c03b16
Logan Connor <logan.connor@floridadep.gov>
2020-04-03 12:20:17 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/to-do-ionic-react/precache-manifest.0cf4f2c130b3337bbfe5d5f18dd03432.js"
+  "/to-do-ionic-react/precache-manifest.c8668573bb1bc8f5e76889c2050b48b5.js"
 );
 
 self.addEventListener('message', (event) => {
